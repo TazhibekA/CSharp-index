d0d27dc2d8b780a38086eed20f00058833a65fca
TazhibekA <40587394+TazhibekA@users.noreply.github.com>
2018-11-13 20:30:02 +0600

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/OneDimArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    public class OneDimArray
+    {
+        public int[] Numbers { get; set; }
+        public int Length { get; set; }
+
+        public OneDimArray(int length)
+        {
+            Length = length;
+            Numbers = new int[length];
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                return Numbers[i];
+            }
+            set
+            {
+
+                Numbers[i] = value * value;
+            }
+        }
+
+    
+    }
+}
---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            OneDimArray oneDimArray = new OneDimArray(2);
+            oneDimArray[0] = 5;
+            Console.WriteLine(oneDimArray[0]);
+            Console.ReadLine();
+        }
+    }
+}
